ce2f41e6f5e964b59848b1c1c9b2e9dd98a57f41
CinnamonShrine <marifbillah524@gmail.com>
2021-01-02 14:38:28 +0700

    Add switch case for conversion output

--- src/Konversi/dectobin_bintodec.java
+++ src/Konversi/dectobin_bintodec.java
@@ -41,6 +41,26 @@ public class dectobin_bintodec {
         System.out.println(" 1. Desimal ke Biner\n 2. Biner ke Desimal\n");
         System.out.print("Masukkan Pilihan: ");
         Scanner input = new Scanner(System.in);
+        
+        switch (input.nextInt()){
+            case 1:
+                System.out.println("\n= Desimal ke Biner =");
+                System.out.print("Masukkan bil.Desimal: ");
+                Scanner desimal = new Scanner(System.in);
+                String biner =
+                Integer.toBinaryString(desimal.nextInt());
+                System.out.println("Hasil: "+biner);
+                System.out.print("\n");
+                break;
+            
+            default:
+                System.out.println("\n= Biner ke Desimal =");
+                System.out.print("Masukkan bil.Biner: ");
+                Scanner inputBiner = new Scanner(System.in);
+                int result = Integer.parseInt(inputBiner.nextLine(),2);
+                System.out.println("Hasil: "+result); 
+                System.out.println("Desimal: " +bd.binerToDecimal(inputBiner.nextLine()));
+                break;
     }
-    
+    }
 }
